438eb2c540022200c7c8b3df8c803c9af8d3d9e1
kramolla <150857490+kramolla@users.noreply.github.com>
2024-12-04 21:51:07 +0300

    rename 2 OrderModel parameters

--- database/models/OrderModel.py
+++ database/models/OrderModel.py
@@ -6,8 +6,8 @@ class OrderModel(Base):
     __tablename__ = 'orders'
 
     # Внешние ключи на таблицы продуктов и партнеров
-    fk_product_name = Column(Integer, ForeignKey('products.id'), primary_key=True)
-    fk_company_name = Column(Integer, ForeignKey('partners.id'), primary_key=True)
+    fk_product_id = Column(Integer, ForeignKey('products.id'), primary_key=True)
+    fk_company_id = Column(Integer, ForeignKey('partners.id'), primary_key=True)
 
     # Поле для хранения количества товаров в заказе
     quantity_of_products = Column(Integer, nullable=False)
@@ -17,6 +17,6 @@ class OrderModel(Base):
 
     def __repr__(self):
         return (
-            f"<Order(fk_product_name={self.fk_product_name}, fk_company_name='{self.fk_company_name}', "
+            f"<Order(fk_product_name={self.fk_product_id}, fk_company_name='{self.fk_company_id}', "
             f"quantity_of_products={self.quantity_of_products}, date_of_create={self.date_of_create})>"
         )
