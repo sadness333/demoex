3c8b25cf0021c61ebe46fd945f0884105fd784f3
kramolla <150857490+kramolla@users.noreply.github.com>
2024-12-04 16:37:08 +0300

    delete trash

--- database/CRUDs/OrderCRUDs.py
+++ database/CRUDs/OrderCRUDs.py
@@ -4,21 +4,6 @@ from database.models.OrderModel import OrderModel
 
 # CRUD операции
 class OrderCRUD:
-    # @staticmethod
-    # def create_order(product_name: str, partner_name: str, quantity_of_products: int, date_of_create=None):
-    #     """Создает новый заказ."""
-    #     try:
-    #         with session.begin():
-    #             order = OrderModel(
-    #                 product_name=product_name,
-    #                 partner_name=partner_name,
-    #                 quantity_of_products=quantity_of_products,
-    #                 date_of_create=date_of_create,
-    #             )
-    #             session.add(order)
-    #     except SQLAlchemyError as e:
-    #         print(f"Ошибка создания заказа: {e}")
-
     @staticmethod
     def read_orders_by_company_id(company_name):
         """Возвращает список всех заказов конкретного партрнёра."""
